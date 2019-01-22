4cbedbdec781b363caaa96e7a892a8d4cdc396e1
K0NT0L <46758245+K0NT0L@users.noreply.github.com>
2019-01-22 19:32:46 +0700

    Update colly.sh

--- colly.sh
+++ colly.sh
@@ -14,23 +14,19 @@ red='\033[1;91m'
 blue='\033[34;1m'
 green='\033[32;1m'
 
-webdav='''
-\033[1;91m ┌──────────────────┐
-\033[33;1m│▌ ▌▛▀▘▛▀▖▛▀▖ ▞▀▖▌ ▌│
-\033[34;1m│▌▖▌▙▄ ▙▄▘▌ ▌ ▙▄▌▚▗▘│
-\033[32;1m│▙▚▌▌  ▌ ▌ ▌ ▌ ▌ ▌ ▝▞ │
-\033[35;1m│▘ ▘▀▀▘▀▀ ▀▀  ▘ ▘   ▘ │
-\033[36;1m└──────────────────
-\033[34;1mTools Webdav Version 1.1
-\033[32;1mAuthor By Star:v
-\033[3From : SAD Cyber Team
-'''
-
 
 termux-setup-storage
 clear
 clear
-echo $webdav
+echo '\033[1;91m ┌──────────────────┐'
+echo '\033[33;1m│▌ ▌▛▀▘▛▀▖▛▀▖ ▞▀▖▌ ▌│'
+echo '\033[34;1m│▌▖▌▙▄ ▙▄▘▌ ▌ ▙▄▌▚▗▘│'
+echo '\033[32;1m│▙▚▌▌  ▌ ▌ ▌ ▌ ▌ ▌ ▝▞ │'
+echo '\033[35;1m│▘ ▘▀▀▘▀▀ ▀▀  ▘ ▘   ▘ │'
+echo '\033[36;1m└──────────────────'
+echo '\033[34;1mTools Webdav Version 1.1'
+echo '\033[32;1mAuthor By Star:v'
+echo '\033[38;1mFrom : SAD Cyber Team'
 sleep 2 
 echo ""
 read -p "Masukkan web korban  ~# " target;
